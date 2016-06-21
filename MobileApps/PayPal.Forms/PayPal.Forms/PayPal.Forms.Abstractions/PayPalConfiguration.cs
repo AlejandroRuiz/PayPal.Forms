324ff606d5b32d92e99ac65bc845bfc24e740fca
fdesbrosses <fdesbrosses@tekigo.com>
2016-06-21 11:29:58 +0200

    Support for forcing localization and phone number

--- MobileApps/PayPal.Forms/PayPal.Forms/PayPal.Forms.Abstractions/PayPalConfiguration.cs
+++ MobileApps/PayPal.Forms/PayPal.Forms/PayPal.Forms.Abstractions/PayPalConfiguration.cs
@@ -2,25 +2,29 @@
 
 namespace PayPal.Forms.Abstractions
 {
-	public class PayPalConfiguration
-	{
-		public PayPalEnvironment Environment { get; private set; }
+    public class PayPalConfiguration
+    {
+        public PayPalEnvironment Environment { get; private set; }
 
-		public string PayPalKey { get; private set; }
+        public string PayPalKey { get; private set; }
 
-		public string MerchantName { get; set; }
+        public string PhoneCountryCode { get; set; }
 
-		public string MerchantPrivacyPolicyUri { get; set; }
+        public string Language { get; set; }
 
-		public string MerchantUserAgreementUri { get; set; }
+        public string MerchantName { get; set; }
 
-		public bool AcceptCreditCards { get; set; }
+        public string MerchantPrivacyPolicyUri { get; set; }
 
-		public PayPalConfiguration (PayPalEnvironment enviroment, string idEnvironment)
-		{
-			Environment = enviroment;
-			PayPalKey = idEnvironment;
-		}
-	}
+        public string MerchantUserAgreementUri { get; set; }
+
+        public bool AcceptCreditCards { get; set; }
+
+        public PayPalConfiguration(PayPalEnvironment enviroment, string idEnvironment)
+        {
+            Environment = enviroment;
+            PayPalKey = idEnvironment;
+        }
+    }
 }
 
